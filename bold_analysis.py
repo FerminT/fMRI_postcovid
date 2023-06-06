6772b1466fa294b82c59ea70ec6f317afd62b081
FerminT <fermintravi@gmail.com>
2023-06-06 18:58:50 -0300

    Compute time series in script

--- bold_analysis.py
+++ bold_analysis.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 import argparse
-import constants
 from pathlib import Path
 
 # NiLearn methods and classes
@@ -9,6 +8,46 @@ from nilearn import datasets, plotting, image
 from nilearn.interfaces import fmriprep
 from nilearn.connectome import ConnectivityMeasure
 from nilearn.maskers import NiftiLabelsMasker, MultiNiftiMasker
+
+TEMPLATE_SHAPE = [55, 65, 55]
+
+
+def compute_timeseries(subjects_df, conf_strategy, atlas_name):
+    atlas = load_atlas(atlas_name)
+    # Use apply_mask to all subjects in dataframe
+    subjects_df['time_series'] = subjects_df.apply(lambda subj: apply_mask(subj['func_path'], subj['mask_path'],
+                                                                           conf_strategy, atlas.maps), axis=1)
+
+
+def apply_mask(func_data, brain_mask, conf_strategy, atlas_maps):
+    nifti_masker = NiftiLabelsMasker(labels_img=atlas_maps,
+                                     mask_img=brain_mask,
+                                     smoothing_fwhm=6,
+                                     low_pass=0.08,
+                                     high_pass=0.01,
+                                     t_r=2.,
+                                     standardize=False,
+                                     detrend=True,
+                                     memory='nilearn_cache', memory_level=2)
+    confounds, sample_mask = fmriprep.load_confounds_strategy(func_data, conf_strategy)
+    time_series = nifti_masker.fit_transform(func_data, confounds=confounds, sample_mask=sample_mask)
+
+    return time_series
+
+
+def load_atlas(atlas_name):
+    # Use nilearn datasets to fetch atlas
+    if atlas_name == 'aal':
+        atlas = datasets.fetch_atlas_aal()
+    elif atlas_name == 'destrieux':
+        atlas = datasets.fetch_atlas_destrieux_2009()
+    elif atlas_name == 'schaefer':
+        atlas = datasets.fetch_atlas_schaefer_2018()
+    else:
+        raise ValueError(f'Atlas {atlas_name} not recognized')
+
+    return atlas
+
 
 def load_clinical_data(clinical_datafile):
     cg = pd.read_csv(clinical_datafile)
@@ -19,17 +58,31 @@ def load_clinical_data(clinical_datafile):
     subjects_data = subjects_data.drop(subjects_data[subjects_data['AnonID'].isin(subjects_to_remove)].index)
     # Remove subjects with no cluster
     subjects_data = subjects_data[~subjects_data['cluster'].isna()]
+    subjs_by_cluster = divide_by_cluster(subjects_data)
 
     return subjects_data
+
+
+def load_datapaths(subjects_paths, subjects_df):
+    for subj_path in subjects_paths:
+        subj_id = int(subj_path.name.split('-')[1])
+        if subj_id in subjects_df['AnonID'].values:
+            # Get the path to the preprocessed functional data
+            func_path = subj_path / 'func'
+            func_file = [f for f in func_path.glob('*.nii.gz') if 'preproc' in f.name][0]
+            mask_file = [f for f in func_path.glob('*.nii.gz') if 'brain_mask' in f.name][0]
+            subjects_df.loc[subjects_df['AnonID'] == subj_id, 'func_path'] = str(func_file)
+            subjects_df.loc[subjects_df['AnonID'] == subj_id, 'mask_path'] = str(mask_file)
 
 
 def divide_by_cluster(subjects_df):
     # Get subjects per cluster
     clusters = subjects_df['cluster'].unique()
-    subjs_per_cluster = {int(cluster): subjects_df[subjects_df['cluster'] == cluster]['AnonID'].values
-                         for cluster in clusters}
+    subjs_by_cluster = {int(cluster): subjects_df[subjects_df['cluster'] == cluster]['AnonID'].values
+                        for cluster in clusters}
 
-    return subjs_per_cluster
+    return subjs_by_cluster
+
 
 if __name__ == '__main__':
     arg_parser = argparse.ArgumentParser()
@@ -56,4 +109,3 @@ if __name__ == '__main__':
         subjects = [sub for sub in derivatives.glob('sub-*') if sub.is_dir()]
     else:
         subjects = [derivatives / f'sub-{args.subject.zfill(3)}']
-
--- constants.py
+++
@@ -1,7 +0,0 @@
-# Define filenames
-BOLD_SUFFIX = '_task-rest_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz'
-AROMA_SUFFIX = '_task-rest_space-MNI152NLin6Asym_desc-smoothAROMA_bold.nii.gz'
-CONFOUNDS_SUFFIX = '_task-rest_desc-confounds_timeseries.tsv'
-BRAIN_MASK_SUFFIX = '_task-rest_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz'
-
-TEMPLATE_SHAPE = [55, 65, 55]
