7e62e68041608181e87361f9609396c658d345d6
FerminT <fermintravi@gmail.com>
2023-06-07 15:50:25 -0300

    Plot corr matrices for clusters

--- bold_analysis.py
+++ bold_analysis.py
@@ -13,6 +13,23 @@ from nilearn.maskers import NiftiLabelsMasker, MultiNiftiMasker
 TEMPLATE_SHAPE = [55, 65, 55]
 
 
+def save_clusters_matrices(clusters_conmatrix, atlas, threshold, output):
+    fig, axes = plt.subplots(nrows=1, ncols=len(clusters_conmatrix), figsize=(20, 20))
+    for i, cluster in enumerate(clusters_conmatrix):
+        cluster_conmatrix = clusters_conmatrix[cluster]
+        # Apply first cluster label order to the rest of the clusters for better comparison
+        if i > 0:
+            label_order = [tick.get_text() for tick in axes[0].get_xticklabels()]
+            regions_indices = [atlas.labels.index(region) for region in label_order]
+            cluster_conmatrix = cluster_conmatrix[regions_indices, :][:, regions_indices]
+        else:
+            label_order = atlas.labels
+        reorder = i == 0
+        plot_matrix_on_axis(cluster_conmatrix, label_order, axes[i], threshold, reorder=reorder)
+        axes[i].set_title(f'Cluster {cluster}')
+    fig.savefig(output / 'clusters.png')
+
+
 def build_connectome(subjects_paths, conf_strategy, atlas_name, n_components, clinical_datafile, output, threshold=95):
     subjects_df = load_clinical_data(clinical_datafile)
     load_datapaths(subjects_paths, subjects_df)
@@ -23,9 +40,16 @@ def build_connectome(subjects_paths, conf_strategy, atlas_name, n_components, cl
     subjects_df['connectivity_matrix'] = subjects_df['time_series'].apply(lambda time_series:
                                                                           connectivity_matrix([time_series])[0][0])
 
+    # Compute mean connectivity matrix by cluster
+    clusters_conmatrix = dict.fromkeys(subjects_df['cluster'].unique())
+    for cluster in clusters_conmatrix:
+        cluster_df = subjects_df[subjects_df['cluster'] == cluster]
+        clusters_conmatrix[cluster] = mean_connectivity_matrix(cluster_df['time_series'].values)
+
     output = output / 'connectivity_matrices'
     output.mkdir(exist_ok=True)
     save_connectivity_matrices(subjects_df, atlas, threshold, output)
+    save_clusters_matrices(clusters_conmatrix, atlas, threshold, output)
 
 
 def save_connectivity_matrices(subjects_df, atlas, threshold, output, reorder=False):
@@ -33,21 +57,31 @@ def save_connectivity_matrices(subjects_df, atlas, threshold, output, reorder=Fa
     for subj in subjects_df['AnonID'].values:
         subj_df = subjects_df[subjects_df['AnonID'] == subj]
         connectivity_matrix = subj_df['connectivity_matrix'].values[0]
-        # Compute percentile and apply threshold
-        percentile = np.percentile(connectivity_matrix, threshold)
-        connectivity_matrix[connectivity_matrix < percentile] = 0
-        # Plot and save connectivity matrix
         fig, ax = plt.subplots(figsize=(10, 8))
-        plotting.plot_matrix(connectivity_matrix,
-                             tri='lower',
-                             labels=atlas.labels,
-                             colorbar=True,
-                             vmax=0.8,
-                             vmin=-0.8,
-                             reorder=reorder,
-                             axes=ax)
+        plot_matrix_on_axis(connectivity_matrix, atlas.labels, ax, threshold, reorder=reorder)
         fig.savefig(output / f'subj_{subj}.png')
         plt.close(fig)
+
+
+def plot_matrix_on_axis(connectivity_matrix, labels, ax, threshold, reorder=False):
+    # Compute percentile and apply threshold
+    percentile = np.percentile(connectivity_matrix, threshold)
+    connectivity_matrix[connectivity_matrix < percentile] = 0
+    plotting.plot_matrix(connectivity_matrix,
+                         tri='lower',
+                         labels=labels,
+                         colorbar=True,
+                         vmax=0.8,
+                         vmin=-0.8,
+                         reorder=reorder,
+                         axes=ax)
+
+
+def mean_connectivity_matrix(time_series, kind='correlation'):
+    _, connectivity_measure = connectivity_matrix(time_series, kind)
+    mean_connectivity_matrix = connectivity_measure.mean_
+
+    return mean_connectivity_matrix
 
 
 def connectivity_matrix(time_series, kind='correlation'):
@@ -96,7 +130,7 @@ def load_clinical_data(clinical_datafile):
     subjects_to_remove = [2, 17, 29]
     subjects_data = subjects_data.drop(subjects_data[subjects_data['AnonID'].isin(subjects_to_remove)].index)
     # Remove subjects with no cluster
-    # subjects_data = subjects_data[~subjects_data['cluster'].isna()]
+    subjects_data = subjects_data[~subjects_data['cluster'].isna()]
     # subjs_by_cluster = divide_by_cluster(subjects_data)
 
     return subjects_data
