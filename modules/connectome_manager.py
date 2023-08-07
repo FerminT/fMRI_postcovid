Fix mean calculation of connectomes

modules/connectome_manager.py
@@ -36,7 +36,7 @@ def build_connectome(subjects_df, conf_strategy, atlas,
     for group in groups_connectivity_matrix:
         print(f'\nProcessing group {group} on {atlas.name}...')
         group_df = subjects_df[subjects_df['group'] == group]
-        groups_connectivity_matrix[group] = mean_connectivity_matrix(group_df['time_series'].values)
+        groups_connectivity_matrix[group] = mean_connectivity_matrix(group_df['connectivity_matrix'].values)
         utils.print_connectivity_metrics(groups_connectivity_matrix[group])
 
     save_connectivity_matrices(subjects_df, atlas.labels, conn_output)
@@ -81,10 +81,8 @@ def networks_connectivity_matrix(subj_connectivity_matrix, networks, all_atlas_l
     return networks_connmatrix
 
 
-def mean_connectivity_matrix(time_series, kind='correlation'):
-    connectivity_matrices, connectivity_measure = connectivity_matrix(time_series, kind)
-    mean_connectivity_matrix = connectivity_measure.mean_
-
+def mean_connectivity_matrix(connectivity_matrices):
+    mean_connectivity_matrix = np.mean(connectivity_matrices.tolist(), axis=0)
     q, df = utils.q_test(connectivity_matrices, mean_connectivity_matrix)
     print(f'Q test: {q}; degrees of freedom: {df}')
 